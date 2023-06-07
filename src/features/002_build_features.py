accab90b339f8b16c94619679ca1fd6bb2df1e91
omid-sar <mr.omid.sardari@gmail.com>
2023-06-07 18:58:22 -0400

    update

--- src/features/002_build_features.py
+++ src/features/002_build_features.py
@@ -21,6 +21,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.preprocessing import OneHotEncoder
 
 
 # Create a custom transformer to add channel dimension to the data. Since the data is black and white, we have only one channel.
@@ -40,3 +41,32 @@ class AddChannelDim(BaseEstimator, TransformerMixin):
 feature_pipeline = Pipeline(
     [("Normalize", MinMaxScaler()), ("Reshape", AddChannelDim())]
 )
+# create target pipeline for preprocessing which includes one hot encoding
+target_pipeline = Pipeline([("OneHotEncode", OneHotEncoder())])
+
+# X_train = feature_pipeline.fit_transform(X_train)
+# y_train = target_pipeline.fit_transform(y_train.values.reshape(-1, 1))
+# print(y_train.shape)
+# y_train = target_pipeline.fit_transform(y_train.values.reshape(-1, 1)).toarray()
+
+print(y_train)
+# sort y_train by index
+print(y_train.sort_index())
+print(y_train.shape)
+print(type(y_train))
+# here since y_train is a pandas series, we need to convert it to numpy array and then reshape it to (-1, 1) to make it a 2D array
+print(y_train.values)
+print(y_train.values.shape)
+print(type(y_train.values))
+print(y_train.values.reshape(-1, 1))
+print(y_train.values.reshape(-1, 1).shape)
+print(target_pipeline.fit_transform(y_train.values.reshape(-1, 1)))
+print(target_pipeline.fit_transform(y_train.values.reshape(-1, 1)).shape)
+print(target_pipeline.fit_transform(y_train.values.reshape(-1, 1)).toarray())
+print(target_pipeline.fit_transform(y_train.values.reshape(-1, 1)).toarray().shape)
+# find max value in x_train
+print(X_train.max())
+X_train.shape
+X_train.max(axis=1)
+X_train[1, :].max()
+type(X_train)
