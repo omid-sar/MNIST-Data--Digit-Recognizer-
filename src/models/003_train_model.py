f15f687089dcb5a2ca6e238f8ee4d368f3c5898f
omid-sar <mr.omid.sardari@gmail.com>
2023-06-12 13:14:04 -0400

    update

--- src/models/003_train_model.py
+++ src/models/003_train_model.py
@@ -9,6 +9,8 @@ from tensorflow import keras
 from tensorflow.keras.metrics import Precision, Recall
 import tensorflow.keras.layers as layers  # for building layers
 from keras.utils.vis_utils import plot_model
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from tensorflow.keras.optimizers import Adam
 
 # ----------------------------------3.1 read data-------------------------------
 
@@ -64,11 +66,11 @@ print("Precision: ", precision.result().numpy())
 print("Recall: ", Recall.result().numpy())
 """
 
-# -------------------------- 3.2 Build and Train Network ------------
+# ----------------------------- 3.2 Build Network(Ver1) -------------------------------
 
 
 # 3.2.1 Build model based on Adam optimizer (with default learning rate) and sparse_categorical_crossentropy loss function
-def built_model():
+def build_model():
     inputs = keras.Input(shape=(28, 28, 1))
     x = layers.Conv2D(32, (3, 3), activation="relu")(inputs)
     x = layers.MaxPooling2D((2, 2))(x)
@@ -90,6 +92,103 @@ def built_model():
     return model
 
 
-model = built_model()
-print(built_model().summary())
+model = build_model()
+build_model().summary()
+print(build_model().summary())
 plot_model(model)
+
+
+# ----------------------------- 3.2 Build Network(Ver2) -------------------------------
+def built_model(
+    input_shape=(28, 28, 1),
+    dropout_rates=[0.25, 0.25, 0.5],
+    learning_rate=0.001,
+    optimizer=Adam,
+    early_stopping_patience=10,
+    model_checkpoint_path="model.h5",
+    num_conv_layers=2,
+    num_dense_layers=1,
+    conv_activation_functions=["relu", "relu"],
+    dense_activation_functions=["relu"],
+    pooling_sizes=[(2, 2), (2, 2)],
+):
+    assert (
+        num_conv_layers == len(conv_activation_functions) == len(pooling_sizes)
+    ), "The length of activation functions and pooling sizes lists must equal the number of convolutional layers"
+    assert num_dense_layers == len(
+        dense_activation_functions
+    ), "The length of activation functions list must equal the number of dense layers"
+
+    # Define the input layer
+    inputs = keras.Input(shape=input_shape)
+
+    x = inputs
+    # Define the convolutional layers
+    for i in range(num_conv_layers):
+        x = layers.Conv2D(
+            32 * (2**i), (3, 3), activation=conv_activation_functions[i]
+        )(x)
+        x = layers.MaxPooling2D(pooling_sizes[i])(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Dropout(dropout_rates[i])(x)
+
+    x = layers.Flatten()(x)
+
+    # Define the dense layers
+    for i in range(num_dense_layers):
+        x = layers.Dense(128 * (2**i), activation=dense_activation_functions[i])(x)
+        x = layers.Dropout(dropout_rates[i + num_conv_layers])(x)
+
+    # Define the output layer
+    outputs = layers.Dense(10, activation="softmax")(x)
+
+    # Define the model and compile it
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(
+        optimizer=optimizer(learning_rate),
+        loss="sparse_categorical_crossentropy",
+        metrics=["accuracy"],
+    )
+
+    # Define early stopping and model checkpointing
+    early_stopping = EarlyStopping(
+        monitor="val_loss",
+        patience=early_stopping_patience,
+        mode="min",
+        restore_best_weights=True,
+    )
+    model_checkpoint = ModelCheckpoint(
+        model_checkpoint_path, monitor="val_loss", mode="min", save_best_only=True
+    )
+
+    return model, [early_stopping, model_checkpoint]
+
+
+model = built_model()[0]
+built_model()[0].summary()
+print(built_model()[0].summary())
+plot_model(model)
+
+
+# ----------------------------- 3.3 Train model ---------------------------------
+# 3.3.1 Train model
+history = model.fit()
+
+
+history = model.fit(
+    X_train,
+    y_train,
+    validation_data=(X_val, y_val),
+    epochs=40,
+    batch_size=batch_size,
+    callbacks=[
+        keras.callbacks.EarlyStopping(
+            monitor="val_loss",
+            mode="min",
+            patience=10,
+            min_delta=0.005,
+            restore_best_weights=True,
+        ),
+        keras.callbacks.ReduceLROnPlateau(monitor="val_loss", patience=3),
+    ],
+)
