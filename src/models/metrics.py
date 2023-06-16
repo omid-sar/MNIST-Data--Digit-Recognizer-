656506e786ce30d0d21f212aa46d3817f65c3c1e
omid-sar <mr.omid.sardari@gmail.com>
2023-06-16 16:49:43 -0400

    update

---
+++ src/models/metrics.py
@@ -0,0 +1,24 @@
+import tensorflow.keras.backend as K
+
+# Define some custom metrics, to help aid us in evaluating model performance. F1-score, Recall and precision
+# are not pre-defined metrics avaliable in keras, which is why we have to manually create them ourselves.
+
+
+def recall_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+
+def precision_m(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+
+def f1_m(y_true, y_pred):
+    precision = precision_m(y_true, y_pred)
+    recall = recall_m(y_true, y_pred)
+    return 2 * ((precision * recall) / (precision + recall + K.epsilon()))
